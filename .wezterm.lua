1a4377413fb01b8c521e60a961a42a623869496e
Joshua Hollander <joshua.hollander@diversus.com.au>
2024-01-25 10:42:19 +0800

    change theme

--- .wezterm.lua
+++ .wezterm.lua
@@ -13,7 +13,8 @@ end
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Belge (terminal.sexy)'
+-- config.color_scheme = 'Belge (terminal.sexy)'
+config.color_scheme = "Tokyo Night Storm"
 
 -- Spawn a pwsh shell
 config.default_prog = { 'pwsh' }
