7dff41559d5434a3a8cffb05228e281616ce3d84
Joshua Hollander <joshua.hollander@diversus.com.au>
2024-02-14 11:42:44 +0800

    Update powershell

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,24 @@
-# Set Node Version
-fnm env --use-on-cd | Out-String | Invoke-Expression 
+# Check once a day if a new powershell update exists
+function GetPowerShellUpdate
+{
+  $currDate = Get-Date -Format "MM-dd-yyyy"
+  try
+  { 
+    $lastChecked = Get-Content "~/.pwshCache" -ErrorAction Stop
+    if ($lastChecked -and [datetime]$currDate -gt [datetime]$lastChecked)
+    {
+      winget upgrade --id Microsoft.Powershell # Update powershell (if required)
+    }
+  } finally
+  { 
+    $currDate > "~/.pwshCache"
+  }
+}
 
 # '...' cd's to either a repo root, or ~
 function GoToGitRoot
 {
-  cd (git rev-parse --show-toplevel 2>$null) || "~"
+  Set-Location (git rev-parse --show-toplevel 2>$null) || "~"
 }
 
 function runEza([string]$path = ".")
@@ -12,6 +26,10 @@ function runEza([string]$path = ".")
   eza $path --icons=always -a --group-directories-first 
 }
 
+#############################################################
+
+# Set Node Version
+fnm env --use-on-cd | Out-String | Invoke-Expression 
 
 # Aliases
 Set-Alias -Name ... GoToGitRoot
@@ -19,5 +37,6 @@ Set-Alias -Name lg lazygit
 Set-Alias -Name ls runEza
 
 # Run Starship
+GetPowerShellUpdate
 Clear-Host
 Invoke-Expression (&starship init powershell)
